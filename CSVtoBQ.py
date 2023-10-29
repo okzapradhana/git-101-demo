0d4fd348af4a0e6213a920fadb4daf8f5979f075
okzapradhana <okzamahendra29@gmail.com>
2023-10-29 12:00:05 +0800

    Create initial csv to big query

---
+++ CSVtoBQ.py
@@ -0,0 +1,15 @@
+import CommonETL
+
+class CSVtoBQ(CommonETL):
+    def __init__(self, filepath):
+        super.__init__(filepath)
+        self.filepath = filepath
+    
+    def extract():
+        pass
+
+    def transform():
+        pass
+
+    def load():
+        pass
