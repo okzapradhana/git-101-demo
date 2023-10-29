5e31e656f41e82f6dedceae12f8f20351d5c0432
okzapradhana <okzamahendra29@gmail.com>
2023-10-29 11:52:04 +0800

    Create new class called CommonETL, that contains: - Extract - Transform - Load

---
+++ CommonETL.py
@@ -0,0 +1,12 @@
+class CommonETL:
+    def __init__(self, data):
+        self.data = data
+    
+    def extract():
+        pass
+
+    def transform():
+        pass
+
+    def load():
+        pass
